d335a67507b67f2c48625215fa772324f9177247
Dana Elizabeth Wyman <dwyman@compute-1-13.local>
2017-08-08 17:44:01 -0700

    clean_splice_jns.py: Added a function to read in the PacBio sam file

--- clean_splice_jns.py
+++ clean_splice_jns.py
@@ -5,7 +5,7 @@ from optparse import OptionParser
 
 def getOptions():
     parser = OptionParser()
-    parser.add_option("--f", dest = "infile", help = "Input file",
+    parser.add_option("--f", dest = "sam", help = "Input file",
                       metavar = "FILE", type = "string", default = "")
     parser.add_option("--o", dest = "outfile",
                       help = "output file", metavar = "FILE", type = "string", default = "out")
@@ -15,14 +15,21 @@ def getOptions():
 def main():
     options = getOptions()
     
+    header, transcripts = processSAM(options.sam)
+
+def processSAM(sam):
+    # This function extracts the SAM header (because we'll need that later) and creates a Transcript object for every sam transcript. 
+    # Transcripts are returned in a list. 
     header = ""
-    with open(options.infile, 'r') as f:
+    transcripts = []
+    with open(sam, 'r') as f:
         for line in f:
-	    line = line.strip()
+            line = line.strip()
             if line.startswith("@"):
                 header = header + line + "\n"
                 continue
             t = Transcript(line)
-            t.printSpliceJunctions()
-            exit()
+            transcripts.append(t)
+    return header, transcripts
+
 main()
