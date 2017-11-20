f6663e9e6a56646ec460e5ec202efe2460b237ee
Dana Elizabeth Wyman <dwyman@compute-1-13.local>
2017-11-20 11:05:06 -0800

    New version of TranscriptClean: TranscriptClean.py

---
+++ TranscriptClean.py
@@ -0,0 +1,5 @@
+# TranscriptClean Version 2
+# Dana Wyman, 11/20/2017
+# In this version of TranscriptClean, mismatches and microindels in long reads are corrected in a SNP-aware fashion using the reference genome and a VCF file of whitelisted variants. Noncanonical splice junctions can also be corrected using a file of reference splice sites as long as the provided SAM file is splice aware.
+
+
