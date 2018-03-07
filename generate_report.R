4b2ed2b22e4d05adc4b4a5f776067db20fc73065
Dana Elizabeth Wyman <dwyman@compute-1-13.local>
2018-03-07 00:14:28 -0800

    Fixed plot 6

--- generate_report.R
+++ generate_report.R
@@ -91,9 +91,6 @@ main <-function() {
     if (nrow(subset(data, Corrected == "Corrected")) > 0) {
         data_p5 = data
         data_p5[data_p5$ErrorType == "NC_SJ_boundary", "ErrorType"] = "NC_SJ"
-        #data_p5 = within(data_p5, ReasonNotCorrected <- paste('(',ReasonNotCorrected, ')', sep=''))
-        #data_p5 = within(data_p5, Category <- paste(ErrorType,Corrected,ReasonNotCorrected,sep=' '))
-        #data_p5$Category <- gsub(' \\(NA\\)', '', data_p5$Category)
         data_p5$Category = rep("Corrected", nrow(data_p5))
         catOrder = c("Corrected")
         plotcolors = c("skyblue")
@@ -112,11 +109,6 @@ main <-function() {
             catOrder = c(catOrder, "Uncorrected (Too far from annotated junction)")
             plotcolors = c(plotcolors, "orange")   
         }
-        #plotcolors = c("red1", "red4", "red3", "goldenrod1", "darkorange4", "darkorange", "springgreen3", "springgreen4", "skyblue", "navy")
-        #catOrder = c("Deletion Uncorrected (TooLarge)", "Deletion Uncorrected (VariantMatch)", "Deletion Corrected",
-        #             "Insertion Uncorrected (TooLarge)", "Insertion Uncorrected (VariantMatch)", "Insertion Corrected",
-        #             "Mismatch Uncorrected (VariantMatch)", "Mismatch Corrected",
-        #             "NC_SJ Uncorrected (TooFarFromAnnotJn)", "NC_SJ Corrected")   
 
          p5 = ggplot(data_p5, aes(x=ErrorType, fill=factor(Category, levels=catOrder))) + geom_bar(position = "dodge") +
              xlab("") + ylab("Count") + customTheme + scale_fill_manual("",values = plotcolors) +
@@ -125,33 +117,36 @@ main <-function() {
         print(p5)
     }    
     
-    # Plot 5: Percentage of transcripts containing error of a given type before and after TranscriptClean 
-    cmd = paste("wc -l <", logFileVerbose, sep = " ") 
-    totalTranscripts = as.numeric(system(cmd, intern = TRUE)) # get total transcript number from other log file because TE log only records errors
-    print(totalTranscripts)
+    # Plot 6: Percentage of transcripts containing error of a given type before and after TranscriptClean 
+    if (nrow(subset(data, Corrected == "Corrected")) > 0) {
+       # cmd = paste("wc -l <", logFileVerbose, sep = " ") 
+        cmd = paste("grep -v 'NA'", logFileVerbose, " | wc -l ", sep = " ") 
+        totalTranscripts = as.numeric(system(cmd, intern = TRUE)) # get total transcript number from other log file because TE log only records errors
+        print(totalTranscripts)
 
-    transcriptsDBefore = length(unique(subset(data, ErrorType == "Deletion")$TranscriptID)) 
-    transcriptsDAfter = length(unique(subset(data, ErrorType == "Deletion" & Corrected == "Uncorrected")$TranscriptID))
+        transcriptsDBefore = length(unique(subset(data, ErrorType == "Deletion")$TranscriptID)) 
+        transcriptsDAfter = length(unique(subset(data, ErrorType == "Deletion" & Corrected == "Uncorrected")$TranscriptID))
 
-    transcriptsIBefore = length(unique(subset(data, ErrorType == "Insertion")$TranscriptID))
-    transcriptsIAfter = length(unique(subset(data, ErrorType == "Insertion" & Corrected == "Uncorrected")$TranscriptID))
+        transcriptsIBefore = length(unique(subset(data, ErrorType == "Insertion")$TranscriptID))
+        transcriptsIAfter = length(unique(subset(data, ErrorType == "Insertion" & Corrected == "Uncorrected")$TranscriptID))
 
-    transcriptsMBefore = length(unique(subset(data, ErrorType == "Mismatch")$TranscriptID))
-    transcriptsMAfter = length(unique(subset(data, ErrorType == "Mismatch" & Corrected == "Uncorrected")$TranscriptID))
+        transcriptsMBefore = length(unique(subset(data, ErrorType == "Mismatch")$TranscriptID))
+        transcriptsMAfter = length(unique(subset(data, ErrorType == "Mismatch" & Corrected == "Uncorrected")$TranscriptID))
 
-    transcriptsSJBefore = length(unique(subset(data, ErrorType == "NC_SJ_boundary")$TranscriptID))
-    transcriptsSJAfter = length(unique(subset(data, ErrorType == "NC_SJ_boundary" & Corrected == "Uncorrected")$TranscriptID))
+        transcriptsSJBefore = length(unique(subset(data, ErrorType == "NC_SJ_boundary")$TranscriptID))
+        transcriptsSJAfter = length(unique(subset(data, ErrorType == "NC_SJ_boundary" & Corrected == "Uncorrected")$TranscriptID))
 
-    data_p5 = data.frame(ErrorType=c("Deletion", "Insertion", "Mismatch", "NC_SJ"), Before=c(transcriptsDBefore, transcriptsIBefore, transcriptsMBefore, transcriptsSJBefore), After=c(transcriptsDAfter, transcriptsIAfter, transcriptsMAfter, transcriptsSJAfter))
-    data_p5 = melt(data_p5)
-    data_p5$percent = vapply(as.numeric(data_p5$value), percent, numeric(1), totalTranscripts)
-    data_p5$percent = paste(data_p5$percent,"%",sep="")
+        data_p6 = data.frame(ErrorType=c("Deletion", "Insertion", "Mismatch", "NC_SJ"), Before=c(transcriptsDBefore, transcriptsIBefore, transcriptsMBefore, transcriptsSJBefore), After=c(transcriptsDAfter, transcriptsIAfter, transcriptsMAfter, transcriptsSJAfter))
+        data_p6 = melt(data_p6)
+        data_p6$percent = vapply(as.numeric(data_p6$value), percent, numeric(1), totalTranscripts)
+        data_p6$percent = paste(data_p6$percent,"%",sep="")
 
-    p5 = ggplot(data=data_p5, aes(x=ErrorType, y=value, fill = variable)) + geom_bar(stat="identity",position="dodge") +
-        xlab("Error Type") + ylab("Number of transcripts containing at least one error") + customTheme + scale_fill_manual("", values = c("skyblue", "navy")) + 
-        ggtitle("Transcripts containing a given error type before and after \nTranscriptClean correction") + 
-        geom_text(data=data_p5, aes(label=percent), position=position_dodge(width=0.9), vjust=-0.25)
-    print(p5)
+        p6 = ggplot(data=data_p6, aes(x=ErrorType, y=value, fill = variable)) + geom_bar(stat="identity",position="dodge") +
+            xlab("Error Type") + ylab("Number of transcripts containing at least one error") + customTheme + scale_fill_manual("", values = c("skyblue", "navy")) + 
+            ggtitle("Transcripts containing a given error type before and after \nTranscriptClean correction") + 
+            geom_text(data=data_p6, aes(label=percent), position=position_dodge(width=0.9), vjust=-0.25)
+        print(p6)
+    }
     dev.off()
 
 }
