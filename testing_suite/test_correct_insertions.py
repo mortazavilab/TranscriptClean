efd8d4a5ea973f70e5218d2ef0d2cfab6071be14
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-06-17 15:50:43 -0700

    Toy example for insertion testing

---
+++ testing_suite/test_correct_insertions.py
@@ -0,0 +1,70 @@
+import pytest
+from pyfasta import Fasta
+import sys
+sys.path.append("..")
+import transcript2 as t2
+import TranscriptClean as TC
+@pytest.mark.unit
+
+class TestInsertionCorr(object):
+
+    def test_correctable_insertion(self):
+        """ Toy transcript with sequence AAATTGA, where the Ts are a 2 bp insertion.
+            chr1: 202,892,094 - 202,892,098. Insertion is at 202,892,096 """
+
+        sam_fields = "\t".join(["test_read", "0", "chr1", "202892094", "255", "3M2I2M", "*",
+                      "0", "0", "AAATTGA", "*",	"NM:i:2", "MD:Z:5", "jI:B:i,-1",
+                      "jM:B:c,-1" ])
+
+        genome = Fasta("input_files/hg38_chr1.fa")
+        maxLen = 5
+        spliceAnnot = None
+        variants = {}
+        transcriptErrorLog = open("scratch/TE.log", 'w')
+    
+        # Init transcript object
+        transcript = t2.Transcript2(sam_fields, genome, spliceAnnot)
+        transcripts = {}
+        transcripts["test_read"] = transcript       
+
+        # Run correction
+        TC.correctInsertions(transcripts, genome, variants, maxLen, transcriptErrorLog)
+    
+        # Check to see if correction was successful
+        corr = transcripts["test_read"]
+        assert corr.SEQ == "AAAGA"
+        assert corr.CIGAR == "5M"
+
+        transcriptErrorLog.close()
+
+    def test_not_correctable_insertion(self):
+        """ Toy transcript with sequence AAATTGA, where the Ts are a 2 bp insertion.
+            chr1: 202,892,094 - 202,892,098. Insertion is at 202,892,096 """
+
+        sam_fields = "\t".join(["test_read", "0", "chr1", "202892094", "255", "3M2I2M", "*",
+                      "0", "0", "AAATTGA", "*", "NM:i:2", "MD:Z:5", "jI:B:i,-1",
+                      "jM:B:c,-1" ])
+
+        genome = Fasta("input_files/hg38_chr1.fa")
+        maxLen = 1
+        spliceAnnot = None
+        variants = {}
+        transcriptErrorLog = open("scratch/TE.log", 'w')
+
+        # Init transcript object
+        transcript = t2.Transcript2(sam_fields, genome, spliceAnnot)
+        transcripts = {}
+        transcripts["test_read"] = transcript
+
+        # Run correction
+        TC.correctInsertions(transcripts, genome, variants, maxLen, transcriptErrorLog)
+
+        # Check to see if correction was successful
+        corr = transcripts["test_read"]
+        assert corr.SEQ == "AAATTGA"
+        assert corr.CIGAR == "3M2I2M"
+
+        transcriptErrorLog.close()
+         
+        
+         
