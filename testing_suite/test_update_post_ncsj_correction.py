9edb20b9b09ae04d660c0b662e865bc393923850
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-06-21 11:17:37 -0700

    Tested update function

---
+++ testing_suite/test_update_post_ncsj_correction.py
@@ -0,0 +1,48 @@
+import pytest
+from pyfasta import Fasta
+import sys
+sys.path.append("..")
+import transcript2 as t2
+import spliceJunction as sj
+import intronBound as ib
+import TranscriptClean as TC
+
+class TestUpdatePostNCSJCorrection(object):
+
+    def test_update(self):
+        """ Toy transcript with sequence A|GAA, where the splice motif
+            is noncanonical but located 2 bp from a canonical splice donor.
+            chr1: 23,071,357 - 23,072,126
+
+        """
+
+        # Process references
+        sjFile = "input_files/test_junctions.txt"
+        outprefix = "scratch/test"
+        donors, acceptors, sjDict = TC.processSpliceAnnotation(sjFile, outprefix)
+        genome = Fasta("input_files/hg38_chr1.fa")
+
+
+        # Init transcript object
+        sam_fields = "\t".join(["test_read", "0", "chr1", "23071357", "255", "1M766N3M", "*",
+                      "0", "0", "AGAA", "*",  "NM:i:0", "MD:Z:4"])
+        transcript = t2.Transcript2(sam_fields, genome, sjDict)
+        jnNumber = 0
+        maxDist = 5
+        donor = (transcript.spliceJunctions[jnNumber]).bounds[0]
+
+        # Attempt to correct the splice donor side of the junction (left)
+        transcript.SEQ, transcript.CIGAR= TC.fix_one_side_of_junction(transcript.CHROM,
+                                                         transcript.POS, jnNumber,
+                                                         donor, 2, genome,
+                                                         transcript.SEQ,
+                                                         transcript.CIGAR)
+        
+        # Now test the update function
+        TC.update_post_ncsj_correction(transcript, jnNumber, genome, sjDict)
+
+        junction = transcript.spliceJunctions[jnNumber]
+        assert junction.motif_code == "21"
+        assert junction.isCanonical == True
+        assert transcript.MD == "MD:Z:6"
+        assert transcript.isCanonical == True
