0ab31cced19783081f5c06661bb54352eef8fc07
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-09-04 11:26:17 -0700

    Completed NM/MD tests and fixed a mistake in the test_insertion_deletion_mismatch_ncsj case (the issue was with the input file not with TranscriptClean)

--- testing_suite/test_compute_MD_NM.py
+++ testing_suite/test_compute_MD_NM.py
@@ -7,6 +7,108 @@ import TranscriptClean as TC
 @pytest.mark.unit
 
 class TestComputeMD(object):
+    def test_perfect_match_no_introns(self):
+        """ Compute the correct MD tag for a transcript that is a perfect 
+            reference match with no introns. """
+        
+        sam = "input_files/sams/perfectReferenceMatch_noIntrons.sam"
+        genome = Fasta("input_files/hg38_chr1.fa")
+
+        with open(sam, 'r') as f:
+            sam_line = f.readline().strip()
+            transcript = t2.Transcript2(sam_line, genome, {})
+            transcript.getNMandMDFlags(genome)
+
+        correct_MD = "MD:Z:155"
+        correct_NM = "NM:i:0"
+        assert transcript.MD == correct_MD
+        assert transcript.NM == correct_NM
+
+    def test_perfect_match_with_introns(self):
+        """ Compute the correct MD tag for a transcript that is a perfect
+            reference match containing introns. """
+
+        sam = "input_files/sams/perfectReferenceMatch_twoIntrons.sam"
+        genome = Fasta("input_files/hg38_chr1.fa")
+
+        with open(sam, 'r') as f:
+            sam_line = f.readline().strip()
+            transcript = t2.Transcript2(sam_line, genome, {})
+            transcript.getNMandMDFlags(genome)
+
+        correct_MD = "MD:Z:3400"
+        correct_NM = "NM:i:0"
+        assert transcript.MD == correct_MD
+        assert transcript.NM == correct_NM
+
+    def test_mismatch(self):
+        """ Compute the correct MD tag for a spliced transcript that contains a
+            mismatch. """
+        
+        sam = "input_files/sams/mismatch.sam"
+        genome = Fasta("input_files/hg38_chr1.fa")
+
+        with open(sam, 'r') as f:
+            sam_line = f.readline().strip()
+            transcript = t2.Transcript2(sam_line, genome, {})
+            transcript.getNMandMDFlags(genome)
+
+        correct_MD = "MD:Z:2G2813"
+        correct_NM = "NM:i:1"
+        assert transcript.MD == correct_MD
+        assert transcript.NM == correct_NM
+
+    def test_deletion(self):
+        """ Compute the correct MD tag for a spliced transcript that contains a
+            deletion. """
+
+        sam = "input_files/sams/deletion.sam"
+        genome = Fasta("input_files/hg38_chr1.fa")
+
+        with open(sam, 'r') as f:
+            sam_line = f.readline().strip()
+            transcript = t2.Transcript2(sam_line, genome, {})
+            transcript.getNMandMDFlags(genome)
+
+        correct_MD = "MD:Z:2629^G377"
+        correct_NM = "NM:i:1"
+        assert transcript.MD == correct_MD
+        assert transcript.NM == correct_NM
+
+    def test_insertion(self):
+        """ Compute the correct MD tag for a spliced transcript that contains an
+            insertion. """
+
+        sam = "input_files/sams/insertion.sam"
+        genome = Fasta("input_files/hg38_chr1.fa")
+
+        with open(sam, 'r') as f:
+            sam_line = f.readline().strip()
+            transcript = t2.Transcript2(sam_line, genome, {})
+            transcript.getNMandMDFlags(genome)
+
+        correct_MD = "MD:Z:3069"
+        correct_NM = "NM:i:2"
+        assert transcript.MD == correct_MD
+        assert transcript.NM == correct_NM
+
+    def test_deletion_insertion_mismatch(self):
+        """ Compute the correct MD tag for a spliced transcript that contains an
+            insertion, deletion, and mismatch. """
+
+        sam = "input_files/sams/deletion_insertion_mismatch.sam"
+        genome = Fasta("input_files/hg38_chr1.fa")
+
+        with open(sam, 'r') as f:
+            sam_line = f.readline().strip()
+            transcript = t2.Transcript2(sam_line, genome, {})
+            transcript.getNMandMDFlags(genome)
+
+        correct_MD = "MD:Z:475C0A0C0C0A1082^C1347G17C205"
+        correct_NM = "NM:i:9"
+        assert transcript.MD == correct_MD
+        assert transcript.NM == correct_NM
+
     def test_insertion_deletion_mismatch_ncsj(self):
         """ Compute the correct MD tag for a transcript that contains an 
            insertion, deletion, mismatch, and noncanonical splice junction in 
@@ -20,6 +122,12 @@ class TestComputeMD(object):
             transcript = t2.Transcript2(sam_line, genome, {})
             transcript.getNMandMDFlags(genome)
 
-        correct_MD = sam_line.split("\t")[14]
+        sam_line = sam_line.split("\t")
+        correct_MD = "MD:Z:414G0A450^C405^C2435"
+        correct_NM = "NM:i:5"
+        ops, cts = transcript.splitCIGAR()
+        for a,b in zip(ops,cts):
+            print(str(a)+str(b))
         assert transcript.MD == correct_MD
+        assert transcript.NM == correct_NM
         
