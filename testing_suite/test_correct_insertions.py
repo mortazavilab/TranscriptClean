aa841516a626973ef6eba9ba17228933ccc5b360
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-09-04 14:47:57 -0700

    Added insertion test

--- testing_suite/test_correct_insertions.py
+++ testing_suite/test_correct_insertions.py
@@ -64,5 +64,32 @@ class TestInsertionCorr(object):
 
         transcriptErrorLog.close()
          
-        
+    def test_variant_insertion(self):
+        """ Toy transcript with sequence AAATTGA, where the Ts are a 2 bp 
+            insertion that matches a known variant.
+            chr1: 202,892,094 - 202,892,098. Insertion is at 202,892,096 """
+
+        sam_fields = "\t".join(["test_read", "0", "chr1", "202892094", "255", "3M2I2M", "*",
+                      "0", "0", "AAATTGA", "*", "NM:i:2", "MD:Z:5", "jI:B:i,-1",
+                      "jM:B:c,-1" ])
+
+        genome = Fasta("input_files/hg38_chr1.fa")
+        maxLen = 1
+        spliceAnnot = None
+        variants = {"chr1_202892096_202892097": "TT"}
+        transcriptErrorLog = open("scratch/TE.log", 'w')
+        logInfo = TC.init_log_info()
+
+        # Init transcript object
+        transcript = t2.Transcript2(sam_fields, genome, spliceAnnot)
+
+        # Run correction
+        TC.correctInsertions(transcript, genome, variants, maxLen, logInfo,
+                             transcriptErrorLog)
+
+        # Check to see if correction was successful
+        assert transcript.SEQ == "AAATTGA"
+        assert transcript.CIGAR == "3M2I2M"
+
+        transcriptErrorLog.close()        
          
