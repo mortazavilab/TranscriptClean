a20fc8dde6db544e7b25f951a354adf0d01942af
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-09-14 22:38:28 -0700

    Passed SNP test. Had to add header to VCF file

---
+++ testing_suite/test_VCF_processing.py
@@ -0,0 +1,29 @@
+import pytest
+#from pyfasta import Fasta
+import sys
+import os
+sys.path.append("..")
+import TranscriptClean as TC
+#import dstruct as dstruct
+@pytest.mark.integration
+
+class TestProcessVCF(object):
+    def test_process_snps(self):
+        """ Make sure that the processVCF function only returns the SNPs that 
+            overlap the provided SAM read, and that it does not crash."""
+
+        maxLenIndel = 5
+        tmp_dir = "scratch/vcf_test"
+        os.system("mkdir -p %s" % (tmp_dir))
+        variant_file = "input_files/vcf_test/snps.vcf"
+        sam_file = "input_files/vcf_test/read_with_snps.sam"
+        snps, insertions, deletions = TC.processVCF(variant_file,
+                                                    maxLenIndel,
+                                                    tmp_dir,
+                                                    sam_file,
+                                                    add_chr = True)
+
+        assert snps["chr11_237087"] == ["G"]
+        assert "chr11_7992932" not in snps
+        assert "chr18_80259190" not in snps
+        assert "chr18_80259245" not in snps
