bd6298ed4066c562e5abe5bf2354b6b9a5f9dc94
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-09-17 13:21:32 -0700

    Added tmp_sam test

---
+++ testing_suite/test_create_tmp_sam.py
@@ -0,0 +1,29 @@
+import pytest
+import sys
+import os
+import subprocess
+sys.path.append("..")
+import TranscriptClean as TC
+@pytest.mark.unit
+
+class TestCreateTmpSamFile(object):
+    def test_create_tmp_sam(self):
+        """ Create a tmp sam file from the mock header and transcripts provided,
+            and make sure the correct chromosomes were detected. Then, check
+            the order of the lines in the tmp file just to be sure."""
+
+        sam_header = ["HLine1", "HLine2"]
+        sam_transcripts = [ "\t".join(["read1", "mapping", "chr1", "..."]),
+                            "\t".join(["read2", "mapping", "chr2", "..."]) ]
+        tmp_dir = "scratch/tmp_sam_test"                            
+
+        fname, chroms = TC.create_tmp_sam(sam_transcripts, sam_header, tmp_dir, 
+                                          process = "test")
+
+        assert fname == "scratch/tmp_sam_test/split_uncorr_sams/test.sam"
+        assert chroms == set(["chr1", "chr2"])
+
+                    
+
+
+
