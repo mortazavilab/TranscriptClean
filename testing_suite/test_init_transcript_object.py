b02962168c7d880c4600a7656c3f8257f5d19d47
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-09-18 16:26:33 -0700

    Added test for init of logInfo for non-primary read

--- testing_suite/test_init_transcript_object.py
+++ testing_suite/test_init_transcript_object.py
@@ -9,8 +9,8 @@ import dstruct as dstruct
 
 class TestInitTranscript(object):
     def test_unmapped_read(self):
-        """ The supplied read is unmapped. This means that a limited object is 
-            created, and the logInfo struct logs the unmapped status."""
+        """ The supplied read is unmapped. This means that no transcript object is 
+            created, and the logInfo struct notes the unmapped status."""
 
         sam_file = "input_files/init_transcript/unmapped.sam"
         with open(sam_file, 'r') as f:
@@ -22,6 +22,31 @@ class TestInitTranscript(object):
         transcript, logInfo = TC.transcript_init(sam_line, genome, sjAnnot)
         assert transcript == None
         assert logInfo.mapping == "unmapped"
+        assert logInfo.corrected_deletions == \
+               logInfo.uncorrected_deletions == \
+               logInfo.variant_deletions == \
+               logInfo.corrected_insertions == \
+               logInfo.uncorrected_insertions == \
+               logInfo.variant_insertions == \
+               logInfo.corrected_mismatches == \
+               logInfo.uncorrected_mismatches == \
+               logInfo.corrected_NC_SJs == logInfo.uncorrected_NC_SJs == "NA"
+
+    def test_nonprimary_read(self):
+        """ The supplied read is a non-primary alignment. This means that no 
+            transcript object is created, and the logInfo struct notes the 
+            non-primary status."""
+
+        sam_file = "input_files/init_transcript/non-primary.sam"
+        with open(sam_file, 'r') as f:
+            sam_line = f.readline().strip()
+
+        genome = None
+        sjAnnot = None
+
+        transcript, logInfo = TC.transcript_init(sam_line, genome, sjAnnot)
+        assert transcript == None
+        assert logInfo.mapping == "non-primary"
         assert logInfo.corrected_deletions == \
                logInfo.uncorrected_deletions == \
                logInfo.variant_deletions == \
