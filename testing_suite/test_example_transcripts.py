b4086139c9b0f0246f652d5f44463bfcd3721aa4
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-09-19 15:11:45 -0700

    Added full TC run on same transcript

--- testing_suite/test_example_transcripts.py
+++ testing_suite/test_example_transcripts.py
@@ -2,6 +2,7 @@ import pytest
 from pyfasta import Fasta
 import sys
 import os
+import subprocess
 sys.path.append("..")
 import transcript as t2
 import TranscriptClean as TC
@@ -11,7 +12,7 @@ import dstruct as dstruct
 class TestCorrectTranscripts(object):
     def test_DIM_nc(self):
         """ Correct a transcript containing a deletion, insertion, mismatch,
-            and noncanonical splcie junction """
+            and noncanonical splice junction """
 
         # Initialize options etc.
         sam = "input_files/sams/deletion_insertion_mismatch_nc.sam"
@@ -88,6 +89,70 @@ class TestCorrectTranscripts(object):
                                    "1", "0"])
                                   
         with open(tmp_dir + "DIM_nc_clean.log", 'r') as f:
+            log = f.readline().strip()
+            assert log == expected_log
+
+    def test_DIM_nc_full(self):
+        """ Correct a transcript containing a deletion, insertion, mismatch,
+            and noncanonical splice junction, this time by running 
+            TranscriptClean from the top """
+
+        command = ["python", "../TranscriptClean.py", "--sam",
+                   "input_files/sams/deletion_insertion_mismatch_nc.sam",
+                   "--g", "input_files/hg38_chr1.fa", "-j", 
+                   "input_files/GM12878_SJs_chr1.tab", "--maxLenIndel", "5",
+                   "--maxSJOffset", "5", "--correctMismatches", "True",
+                   "--correctIndels", "True", "--correctSJs", "True", "--primaryOnly",
+                   "--o", "scratch/DIM_nc_full/TC"]
+                   
+        try:
+            subprocess.check_output(command)
+
+        except Exception as e:
+            print(e)
+            pytest.fail("TranscriptClean run crashed.")
+
+
+        # Now check the results
+        # Expected transcript attributes post-correction
+        correct_CIGAR = ("12M1134N126M163N202M866N74M924N191M1777N127M2109N"
+                         "157M88N159M932N633M274N117M7696N170M1215N629M938N"
+                         "29M428N133M254N166M390N212M253N89M163N483M")
+        correct_MD = "MD:Z:3709"
+        correct_NM = "NM:i:0"
+        correct_jI = ("jI:B:i,150941429,150942562,150942689,150942851,150943054,"
+                      "150943919,150943994,150944917,150945109,150946885,150947013,"
+                      "150949121,150949279,150949366,150949526,150950457,150951091,"
+                      "150951364,150951482,150959177,150959348,150960562,150961192,"
+                      "150962129,150962159,150962586,150962720,150962973,150963140,"
+                      "150963529,150963742,150963994,150964084,150964246")
+        correct_jM = "jM:B:c,21,21,21,21,21,21,21,21,21,21,21,21,21,21,21,21,21"
+
+        # Read in transcript from outfile
+        with open("scratch/DIM_nc_full/TC_clean.sam", 'r') as f:
+            sam_line = f.readline().strip().split('\t')
+       
+        CIGAR = sam_line[5]
+        MD = sam_line[14]
+        NM = sam_line[13]
+        jI = sam_line[16]
+        jM = sam_line[15] 
+
+        assert CIGAR == correct_CIGAR
+        assert MD == correct_MD
+        assert NM == correct_NM
+        assert jI == correct_jI
+        assert jM == correct_jM
+
+        # Read logs and make sure they are OK
+        expected_log = "\t".join(["c34150/f1p1/3707", "primary",
+                                   "2", "0", "0",
+                                   "1", "0", "0",
+                                   "2", "0",
+                                   "1", "0"])
+
+        with open("scratch/DIM_nc_full/TC_clean.log", 'r') as f:
+            header = f.readline().strip()
             log = f.readline().strip()
             assert log == expected_log
 
