e227b2a612aee22c22bfc1b3f52ea8d97be38ce2
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-09-25 11:20:37 -0700

    Fixed typo

--- testing_suite/unzip_references.sh
+++ testing_suite/unzip_references.sh
@@ -2,7 +2,7 @@
 # Check if fasta files are zipped, and if they are, unzip them
 if [ -f "input_files/hg38_chr1.fa.gz" ] && [ ! -f "input_files/hg38_chr1.fa" ]; then
     echo "Unzipping chr1 fasta file"
-    gunzip input_files/hg38_chr11.fa.gz
+    gunzip input_files/hg38_chr1.fa.gz
 fi
 
 if [ -f "input_files/hg38_chr11.fa.gz" ] && [ ! -f "input_files/hg38_chr11.fa" ]; then
